42c07b0b5b02ec179a226a937118ea83fe88e3a9
adarshreddyash <adarshreddy9849@gmail.com>
2019-07-15 16:37:58 +0530

    api set

--- t.py
+++ t.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request , jsonify
+from flask import Flask,render_template,url_for,request,jsonify
 import pandas as pd 
 import numpy as np
 import json
@@ -6,14 +6,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 
-app = Flask(__name__)
+request = 1
+msg = "hai hello hw are yoi"
 
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-@app.route('/predict/<msg>')
-def predict(msg):
+def predict():
+	#msg = request.args.get('msg')
 	df= pd.read_csv("spam.csv", encoding="latin-1")
 	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
 	# Features and Labels
@@ -26,22 +23,29 @@ def predict(msg):
 	X = cv.fit_transform(X) # Fit the Data
 	from sklearn.model_selection import train_test_split
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-	#Naive Bayes Classifier
+	#naive bayes classsifier
 	from sklearn.naive_bayes import MultinomialNB
 
 	clf = MultinomialNB()
 	clf.fit(X_train,y_train)
 	clf.score(X_test,y_test)
-	file1 = open('ash.txt','w+')
-	file1.write(str(msg))
-	data = file1
-	vect = cv.transform(data).toarray()
-	my_prediction = clf.predict(vect)
-	#print(my_prediction)
-	if np.any(my_prediction) == 1:
-		return jsonify("y")
-	elif np.any(my_prediction) == 0:
-		return jsonify("naa")
+	#not working -flag
+	#Alternative Usage of Saved Model
+	# joblib.dump(clf, 'NB_spam_model.pkl')
+	# NB_spam_model = open('NB_spam_model.pkl','rb')
+	# clf = joblib.load(NB_spam_model)
+   
+	if request  == 1:
+		file1 = open("data.txt","w+") 
+		file1.write("hai hello \n hai hello \n hello")
+		data = file1
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+		file1.close()
+		if np.any(my_prediction) == 1:
+			res = "hai"
+		else:
+			res = "hello"	
+	return res
 
-if __name__ == '__main__':
-	app.run(debug=True)
+predict()
